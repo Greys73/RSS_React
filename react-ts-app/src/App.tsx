82febdb6ccb9f3744561923b81201c136207f430
Greys73 <v.myazin@gmail.com>
2023-10-26 19:34:53 +0400

    feat: create base Fetch logic Thu, Oct 26, 2023  7:34:53 PM

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,5 +1,12 @@
+import Search from './Search';
+
 function App() {
-  return <h1> Hello World! </h1>;
+  return (
+    <>
+      <h1> Hello World! </h1>
+      <Search />
+    </>
+  );
 }
 
 export default App;
---
+++ react-ts-app/src/Search.tsx
@@ -0,0 +1,52 @@
+import { Component, ReactNode } from 'react';
+import * as Type from './types';
+
+const API_URL = 'https://rickandmortyapi.com/api/character';
+
+export default class Search extends Component<
+  Type.SearchProps,
+  Type.SearchState
+> {
+  constructor(props: Type.SearchProps) {
+    super(props);
+    this.state = {
+      list: [],
+      isLoaded: false,
+      error: null,
+    };
+  }
+
+  componentDidMount(): void {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          list: result.results,
+          isLoaded: true,
+        });
+      })
+      .catch((error) => {
+        this.setState({ isLoaded: true, error });
+      });
+  }
+
+  render(): ReactNode {
+    const { list, isLoaded, error } = this.state;
+    if (error) {
+      return <p>RESPONSE ERROR</p>;
+    }
+    if (!isLoaded) {
+      return <p>LOADING...</p>;
+    }
+    return (
+      <>
+        {(list as Type.TCharacter[]).map((item) => (
+          <>
+            <p key={item.id}>{item.name}</p>
+            <img src={item.image} alt={item.type} />
+          </>
+        ))}
+      </>
+    );
+  }
+}
