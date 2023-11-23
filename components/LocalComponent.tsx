919ea4d832956b64e39a906aab62c8a6781e3ea4
Greys73 <v.myazin@gmail.com>
2023-11-23 23:07:09 +0400

    feat: remove src dir Thu, Nov 23, 2023 11:07:09 PM

---
+++ components/LocalComponent.tsx
@@ -0,0 +1,14 @@
+'use local'
+
+import React from 'react';
+
+const LocalComponent = () => {
+  console.log('local');
+  return (
+    <div>
+      I am local!
+    </div>
+  );
+};
+
+export default LocalComponent;
---
+++ components/ServerComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServerComponent = () => {
+  console.log('server');
+  return (
+    <div>
+      I am from SERVER
+    </div>
+  );
+};
+
+export default ServerComponent;
